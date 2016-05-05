d80088d21258d2339465fb1dcd8037a58c0ca328
DanishKhan14 <danish.belief@gmail.com>
2016-05-05 14:19:06 -0500

    update

--- align.py
+++ align.py
@@ -19,9 +19,9 @@ def runAligner():
     db = raw_input("Enter the community db file path...\n")
     outfile = raw_input("Enter the output file path...\n")
     print "Running Command:\n"
-    print "~/bowtie/bowtie-1.1.2$ ./bowtie -v 2 -a -m 3 --best --strata Community_db --suppress 1,2,4,5,6,7 fastqFile.fastq > outPutFile"
+    print "~/bowtie/bowtie-1.1.2/bowtie -v 2 -a -m 3 --best --strata Community_db --suppress 1,2,4,5,6,7 fastqFile.fastq > outPutFile"
 
-    cmdList = ['bowtie','--strata',db,'--suppress','1,2,4,5,6,7',fastqFile,'>',outfile]    
+    cmdList = ['bowtie','--strata',db,'--suppress','1,2,4,5,6,7',fatqFile,'>',outfile]    
     subprocess.call(cmdList) 
 
 def showResults():

681b09d9c4701a5fdc238b4693dec87a0cd6be71
DanishKhan14 <danish.belief@gmail.com>
2016-12-23 12:41:39 -0600

    update

--- align.py
+++ align.py
@@ -5,10 +5,34 @@ import sys
 import csv
 import glob
 import subprocess
+import argparse
 
-def main(argv):
-    db = argv[0]
-    for readFile in argv[1:]:
+#def main(argv):
+def main():    
+    parser = argparse.ArgumentParser(description='Bowtie Aligner Wrapper.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-db', '--database', help='Database name generated using create_db.py .')
+    parser.add_argument('-input', nargs='+', help='List of FASTQ files to be mapped.')
+    parser.add_argument('-e', '--extension', default='', help='File extension to filter by.')
+    parser.add_argument('-m', default='3', help='Suppress all alignments for a particular read or pair if more than <int> reportable alignments exist for it.')
+    parser.add_argument('-v', default='2', help='Report alignments with at most m mismatches.')
+    args = parser.parse_args()
+
+    # Parse paths
+    full_paths = [os.path.join(os.getcwd(), path) for path in args.input]
+    files = set()
+    for path in full_paths:
+        if os.path.isfile(path):
+            files.add(path)
+        else:
+            files |= set(glob.glob(path + '/*' + args.extension))
+
+    for f in files:
+        print f
+    
+    db = args.database
+    #db = argv[0]
+    #for readFile in argv[1:]:
+    for readFile in files:
         baseName = os.path.basename(readFile)
         outfile = "output/" + os.path.splitext(baseName)[0]+".csv"
         umfile = "unmatched/" + os.path.splitext(baseName)[0]+".txt"
@@ -47,26 +71,27 @@ def getNormalizationFactor(db):
 
     genomes = output.split('\n')[3:]
     avgGenomeSize = sum([int(i.split('\t')[-1]) for i in genomes if i])/len(genomes)
-    print avgGenomeSize
 
     for genome in genomes:
         if genome:
-            normFactor[genome.split('\t')[1]] = round(float(genome.split('\t')[-1])/avgGenomeSize, 4)
+            normFactor[genome.split('\t')[1]] = round(avgGenomeSize/float(genome.split('\t')[-1]), 4)
 
-    print normFactor
     return normFactor
 
 
 def runAligner(index, readFile, outfile, umfile, suppressfile):
     print "Running Bowtie Aligner \n"
     fatqFile = readFile
+    f = open("stats/alignerStats.txt", "a")
     
     print "Running Command:\n"
     print "~/bowtie/bowtie-1.1.2/bowtie -v 2 -a -m 3 --best --strata Community_db --suppress 1,2,4,5,6,7 fastqFile.fastq > outPutFile"
 
     cmdList = ['./bowtie', '-v', '2', '-a', '-m', '3', '--best', '--strata', '--un', umfile, '--max', suppressfile, index,'--suppress','1,2,4,5,6,7',fatqFile, outfile]    
-    subprocess.call(cmdList)
+    subprocess.call(cmdList, stdout=f)
+    f.close()
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    #main(sys.argv[1:])
+    main()

2b9908d588ead0d875a9030975df0543362b1613
DanishKhan14 <danish.belief@gmail.com>
2016-12-26 13:45:37 -0600

    updated install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ unamestr=`uname`
 if [[ -d ./bowtie ]]; then
     echo "Bowtie Already Exists ...Updating Wrapper Scripts Only ...."
 elif [[ "$unamestr" == 'Linux' ]]; then
-    wget https://sourceforge.net/projects/bowtie-bio/files/bowtie/1.1.2/bowtie-1.1.2-macos-x86_64.zip && unzip bowtie-1.1.2-macos-x86_64.zip
-    rm -rf bowtie-1.1.2-macos-x86_64.zip
+    wget https://sourceforge.net/projects/bowtie-bio/files/bowtie/1.1.2/bowtie-1.1.2-linux-x86_64.zip && unzip bowtie-1.1.2-linux-x86_64.zip
+    rm -rf bowtie-1.1.2-linux-x86_64.zip
     mv bowtie-1.1.2 bowtie
 elif [[ "$unamestr" == 'Darwin' ]]; then
     wget https://sourceforge.net/projects/bowtie-bio/files/bowtie/1.1.2/bowtie-1.1.2-macos-x86_64.zip && unzip bowtie-1.1.2-macos-x86_64.zip
@@ -20,9 +20,8 @@ cd bowtie
 if ! [[ -d ./aligner_RL ]]; then
     git clone https://github.com/DanishKhan14/aligner_RL.git
     cp ./aligner_RL/align.py ./
-    cp ./aligner_RL/align.py ./
+    cp ./aligner_RL/merge.sh ./
     cp ./aligner_RL/create_db.py ./
-    cp ./aligner_RL/output.py ./
     exit;
 fi
 
